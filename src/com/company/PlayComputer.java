e7601925048b69492784dd13f81e9f8565e54449
Steven Huang <steven.charles.huang@gmail.com>
2019-10-04 23:20:54 -0400

    completed basic game logic for PlayComputer

--- src/com/company/PlayComputer.java
+++ src/com/company/PlayComputer.java
@@ -35,6 +35,12 @@ public class PlayComputer extends GameLogic {
     }
 
     void play(Map<String, String> playerPlay, String compyPlay) {
-        System.out.println(playerPlay + " " + compyPlay);
+        if (compyPlay == playerPlay.get("wins")) {
+            System.out.println("Player wins! =D");
+        } else if (compyPlay == playerPlay.get("loses")) {
+            System.out.println("Player loses... =(");
+        } else {
+            System.out.println("It's a draw! You both lose...but really, only you lose...LOL");
+        }
     }
 }
