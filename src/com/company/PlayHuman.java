5b89f2cc2c452fdd80f9cd359015674367cbf24a
Steven Huang <steven.charles.huang@gmail.com>
2019-10-07 13:54:22 -0400

    refactored PlayHuman with new method for more robust player choice logic

--- src/com/company/PlayHuman.java
+++ src/com/company/PlayHuman.java
@@ -21,34 +21,25 @@ public class PlayHuman implements GameFlowInterface {
 
         // Get the play selections from User1 and User2
         String player1Choice = scanner.nextLine().toLowerCase().trim();
+        player1Choice = getPlayerChoice(player1Choice);
 
-        // Refactor this for more usability for player 2
-        System.out.println("Player 2, make your move...(Please type the word)");
+        System.out.println("Player 2, make your move...");
+
         String player2Choice = scanner.nextLine().toLowerCase().trim();
+        player2Choice = getPlayerChoice(player2Choice);
 
         // Switch statement to handle Users' inputs
         switch (player1Choice) {
             case "rock":
-            case "r":
-            case "1":
                 play(gameLogic.getRock(), player2Choice);
                 break;
             case "paper":
-            case "p":
-            case "2":
                 play(gameLogic.getPaper(), player2Choice);
                 break;
             case "scissors":
-            case "s":
-            case "3":
                 play(gameLogic.getScissors(), player2Choice);
                 break;
             default:
-                System.out.println(
-                        "Someone's messed up their inputs...\n" +
-                                "let's try that again...\n" +
-                                "===========================================\n");
-                PlayHuman playHuman = new PlayHuman();
                 break;
         }
 
@@ -80,12 +71,55 @@ public class PlayHuman implements GameFlowInterface {
     // Outputs the result of the game to the Users
     private void play(Map<String, String> player1Play, String player2Play) {
         if (player2Play.equals(player1Play.get("wins"))) {
+            System.out.println("Player 1 played, '" + player1Play.get("type") +
+                    "'. Player 2 played, '" +
+                    player2Play +
+                    "'");
             System.out.println("Player 1 wins! =D\n" + "Sorry, Player 2 =(");
         } else if (player2Play.equals(player1Play.get("loses"))) {
+            System.out.println("Player 1 played, '" + player1Play.get("type") +
+                    "'. Player 2 played, '" +
+                    player2Play +
+                    "'");
             System.out.println("Player 2 wins! =D\n" + "Sorry, Player 1 =(");
         } else {
+            System.out.println("Player 1 played, '" + player1Play.get("type") +
+                    "'. Player 2 played, '" +
+                    player2Play +
+                    "'");
             System.out.println("It's a draw! You both lose...LOL");
         }
+    }
+
+    // Get Player 1 choice
+    private String getPlayerChoice(String playerChoice) {
+        String choice = "";
+        // Switch statement to handle Users' inputs
+        switch (playerChoice) {
+            case "rock":
+            case "r":
+            case "1":
+                choice = gameLogic.getRock().get("type");
+                break;
+            case "paper":
+            case "p":
+            case "2":
+                choice =  gameLogic.getPaper().get("type");
+                break;
+            case "scissors":
+            case "s":
+            case "3":
+                choice = gameLogic.getScissors().get("type");
+                break;
+            default:
+                System.out.println(
+                        "Someone's messed up their inputs...\n" +
+                                "let's try that again...\n" +
+                                "===========================================\n");
+                PlayHuman playHuman = new PlayHuman();
+                break;
+        }
+        return choice;
     }
 }
 
